869d51ca7ea12bbc1bd16a0929103901f0c6fbaf
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2008-09-25 17:55:02 +0000

    optimized live page, faster pagination

--- trunk/code/forums.live.php
+++ trunk/code/forums.live.php
@@ -7,61 +7,38 @@ $fItems->initData($typeId,$user->gid,true);
 
 if($typeId=='blog') $fItems->addWhere('i.itemIdTop is null');
  
-$totalItems = 100;
-
 $fItems->setOrder('i.dateCreated desc');
 
-$pager = fSystem::initPager($totalItems,LIVE_PERPAGE);
-$od = ($pager->getCurrentPageID()-1) * LIVE_PERPAGE;
+$pager = fSystem::initPager(0,LIVE_PERPAGE,array('noAutoparse'=>1));
+$from = ($pager->getCurrentPageID()-1) * LIVE_PERPAGE;
 
-$fItems->setLimit($od,LIVE_PERPAGE);
+$fItems->setLimit($from,LIVE_PERPAGE+1);
 
 $fItems->getData();
 
-$tmptext='';
+$totalItems = count($fItems->arrData);
+        
+$maybeMore = false;
+if($totalItems > LIVE_PERPAGE) $maybeMore = true;
 
-$x=0;
-while ($fItems->arrData) {
-  $fItems->parse();
+if($from > 0) {
+    $totalItems += $from;
 }
 
-$tmptext .= '<div class="hfeed">'.$fItems->show().'</div>';
+unset($fItems->arrData[(count($fItems->arrData)-1)]);
 
-if ($totalItems>LIVE_PERPAGE) $tmptext.='<div class="pager">'.$pager->links.'</div>';
+if($totalItems > 0) {
+    $pager->totalItems = $totalItems;
+	$pager->maybeMore = $maybeMore;
+	$pager->getPager();
 
-$TOPTPL->addTab(array("MAINDATA"=>$tmptext));
-
-
-
-/*
-$typeId = $user->currentPage['typeIdChild'];
-
-$fItems = new fItems();
-$fItems->showPageLabel = true;
-$fItems->initData($typeId,$user->gid,true);
-
-//$fItems->debug = 1;
-if($typeId=='blog') $fItems->addWhere('i.itemIdTop is null');
-$totalItems = $fItems->getCount();
-if($totalItems>300) $totalItems = 300;
-
-$fItems->setOrder('i.dateCreated desc');
-
-$pager = fSystem::initPager($totalItems,LIVE_PERPAGE);
-$od = ($pager->getCurrentPageID()-1) * LIVE_PERPAGE;
-
-$fItems->setLimit($od,LIVE_PERPAGE);
-
-$fItems->getData();
-
-$tmptext='';
-if ($totalItems>LIVE_PERPAGE) $tmptext.='<div class="pager">'.$pager->links.'</div>';
-
-while ($fItems->arrData) $fItems->parse();
-
-$tmptext .= '<div class="hfeed">'.$fItems->show().'</div>';
-if ($totalItems>LIVE_PERPAGE) $tmptext.='<div class="pager">'.$pager->links.'</div>';
-
-$TOPTPL->addTab(array("MAINDATA"=>$tmptext));
-*/
-?>
+    while ($fItems->arrData) {
+      $fItems->parse();
+    }
+    
+    $tmptext = '<div class="hfeed">'.$fItems->show().'</div>';
+    
+    if ($totalItems>LIVE_PERPAGE) $tmptext .= $pager->links;
+    
+    $TOPTPL->addTab(array("MAINDATA"=>$tmptext));
+}
