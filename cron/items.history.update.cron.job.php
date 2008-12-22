7679589fe5d18cc2eb2a77eaaf925c8cd5876f6a
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2008-12-22 17:01:10 +0000

    security set, new cron job to refresh item history

---
+++ cron/items.history.update.cron.job.php
@@ -0,0 +1,65 @@
+<?php
+chdir("../");
+require("local.php");
+$nonUserInit = true;
+$nonDbInit = true;
+require(INIT_FILENAME);
+
+if (!$db = mysql_connect($conf['db']['hostspec'], $conf['db']['username'], $conf['db']['password'])) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+
+if (!mysql_select_db($conf['db']['database'], $db)) {
+    echo 'Could not select database';
+    exit;
+}
+
+$queries = array(
+"select date_format(dateCreated,'%Y') as dateCon,itemId,3,count(itemId) as sumVal from sys_pages_items_hit where date_format(dateCreated,'%Y') >= date_format(now(),'%Y') group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y') as dateCon,itemId,4,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y') >= date_format(now(),'%Y') and userId>0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m') as dateCon,itemId,3,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%m') >= date_format(now(),'%Y%m') group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m') as dateCon,itemId,4,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%m') >= date_format(now(),'%Y%m') and userId>0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m-%d') as dateCon,itemId,3,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%m%d') >= date_format(now(),'%Y%m%d') group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m-%d') as dateCon,itemId,3,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%m%d') >= date_format(now(),'%Y%m%d') and userId>0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-W%U') as dateCon,itemId,3,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%U') >= date_format(now(),'%Y%U') group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-W%U') as dateCon,itemId,3,count(itemId) from sys_pages_items_hit where date_format(dateCreated,'%Y%U') >= date_format(now(),'%Y%U') and userId>0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y') as dateCon,itemId,1,count(itemId) from sys_pages_items_tag where date_format(dateCreated,'%Y') >= date_format(now(),'%Y') and weight > 0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m') as dateCon,itemId,1,count(itemId) from sys_pages_items_tag where date_format(dateCreated,'%Y%m') >= date_format(now(),'%Y%m') and weight > 0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-%m-%d') as dateCon,itemId,1,count(itemId) from sys_pages_items_tag where date_format(dateCreated,'%Y%m%d') >= date_format(now(),'%Y%m%d') and weight > 0 group by dateCon,itemId"
+,"select date_format(dateCreated,'%Y-W%U') as dateCon,itemId,1,count(itemId) from sys_pages_items_tag where date_format(dateCreated,'%Y%U') >= date_format(now(),'%Y%U') and weight > 0 group by dateCon,itemId"
+);
+
+$page = 3000;
+$from = 0;
+
+foreach($queries as $query) {
+  $res = 0;
+  $totalTotal = 0;
+  $from = 0;
+  do {
+    $dot = $query.' limit '.$from.','.($page+1);
+    $result = mysql_query($dot,$db);
+    $total = mysql_num_rows($result);
+    $totalTotal += $total;
+    if($total > 0) {
+      //--- update
+      while($row = mysql_fetch_row($result)) {
+        $ins = "insert into sys_pages_items_history values ('".$row[0]."','".$row[1]."','".$row[2]."','".$row[3]."') on duplicate key update valueSum = '".$row[2]."'";
+        $log = $row[0].",".$row[1].",".$row[2];
+        mysql_unbuffered_query($ins,$db);
+        $res++;
+      }
+      $from += $page;
+    }
+    mysql_free_result($result);
+
+  } while ($total > $page);
+  
+  $cron = "insert into sys_cron_log values (now(),'history::".$log."::".$totalTotal.':'.$res."')";
+    mysql_query($cron,$db);  
+  
+}
+
+
+
