dfd4113249b6c46df46013045a066e2f17b546d3
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2009-05-27 17:18:57 +0000

    sorting js

--- trunk/js/UserPost.js
+++ trunk/js/UserPost.js
@@ -1,29 +1,19 @@
 $(document).ready(function(){
-	//global vars
-	var recCombo = $("#recipientcombo");
-	var recInput = $("#prokoho");
-	var recAvatarDiv = $("#recipientavatar");
-
-	recInput.change(avatarfrominput);
-  
-  recCombo.change(function (evt) {
+	$("#prokoho").change(avatarfrominput);
+  $("#recipientcombo").change(function (evt) {
     var str = "";
     $("#recipientcombo option:selected").each(function () { str += $(this).text() + " "; });
-    recInput.attr("value", str);
-    recCombo.attr("selectedIndex", 0);
+    $("#prokoho").attr("value", str);
+    $("#recipientcombo").attr("selectedIndex", 0);
     avatarfrominput(evt);
   });
-  
-  
 });
 
 function avatarfrominput(evt) {
-
     addXMLRequest('username', $("#prokoho").attr("value"));
     addXMLRequest('result', "recipientavatar");
     addXMLRequest('resultProperty', 'html');
     addXMLRequest('call', 'initSupernote');
     addXMLRequest('call', 'initSwitchFriend');
     sendAjax('post-avatarfrominput');
-
 }
