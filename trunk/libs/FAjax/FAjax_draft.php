35b900bd81caa43b0763bfb0155d42da1ae233c9
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2009-05-27 23:27:17 +0000

    all ajax fc ready to testing, TODO:replace xajax calls hard coded -> unobtrusive, create alternatives for all calls ready

--- trunk/libs/FAjax/FAjax_draft.php
+++ trunk/libs/FAjax/FAjax_draft.php
@@ -1,18 +1,9 @@
 <?php
 class FAjax_draft {
-  static function save($data) {
+	static function save($data) {
 
 		FUserDraft::save($data['place'],$data['text']);
-		
-		//---create response
-		$objResponse->call('draftSaved', $place);
-		
-		
-			$retData[] = array('target'=>$data['result'],'property'=>$data['resultProperty'],'value'=>$ret);
-			unset($data['result']);
-			unset($data['resultProperty']);
-			return FAjax::buildResponse($retData, $data);
-		
+
 	}
-	
+
 }
