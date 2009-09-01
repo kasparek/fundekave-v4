43b0b34f0193e8a930d7d8879804b8edeb056206
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2009-09-01 20:39:06 +0000

    dev commit

---
+++ src/files.php
@@ -0,0 +1,30 @@
+<?php
+$data = $_POST['data'];
+$seq = (int)  $_POST['seq'];
+$total = (int)  $_POST['total'];
+$filename = $_POST['filename'];
+if(!empty($data)) {
+
+file_put_contents('chunks/chunk-'.$filename.'-'.$seq.'.txt',$data);
+
+}
+
+$allExists = true; 
+for($i=0;$i<$total;$i++) {
+if(!file_exists('chunks/chunk-'.$filename.'-'.$i.'.txt'))  {
+$allExists = false;
+}
+}
+
+
+if($allExists === true) {
+	//--concat all files
+	$encData = '';
+	for($i=0;$i<$total;$i++) {
+	 $file = 'chunks/chunk-'.$filename.'-'.$i.'.txt';
+		$encData .= trim(file_get_contents($file));
+		//unlink($file);
+	}
+
+	file_put_contents($filename.'.jpg',base64_decode( $encData ));
+}
