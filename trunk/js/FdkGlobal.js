33717d0af697317735ca12d1787e412fe28beca5
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2010-08-22 18:43:46 +0000

    conflict solved

--- trunk/js/FdkGlobal.js
+++ trunk/js/FdkGlobal.js
@@ -283,14 +283,14 @@ function fsubmit(event) {
 	sendAjax(gup('m', this.action),gup('k', this.action));
 };
 
-function fuupInit() { $(".fuup").each(function(i){ swfobject.embedSWF("http://fundekave.net/assets/load.swf", $(this).attr('id'), "120", "25", "10.0.12", "expressInstall.swf", {file:"http://fundekave.net/assets/Fuup.swf",config:"files.php?k="+gup('k',$(".fajaxform").attr('action'))+"|f=cnf|c="+$(this).attr('id').replace(/D/g,".").replace(/S/g,'/'),containerId:$(this).attr('id')},{wmode:'transparent',allowscriptaccess:'always'}); }); }
+function fuupInit() { $(".fuup").each(function(i){ swfobject.embedSWF(ASSETS_URL+"load.swf", $(this).attr('id'), "120", "25", "10.0.12", ASSETS_URL+"expressInstall.swf", {file:ASSETS_URL+"Fuup.swf",config:"files.php?k="+gup('k',$(".fajaxform").attr('action'))+"|f=cnf|c="+$(this).attr('id').replace(/D/g,".").replace(/S/g,'/'),containerId:$(this).attr('id')},{wmode:'transparent',allowscriptaccess:'always'}); }); }
 
 
 
 function fajaxa(event) { setListeners('fajaxa', 'click', fajaxaSend); };
 function fajaxaSend(event) {
 	if($(this).hasClass('hash')) document.location.hash = gup('m',this.href)+'/'+gup('d',this.href);
-	if($(this).hasClass('showBusy')) $(".showProgress").attr('src','assets/loading.gif');
+	if($(this).hasClass('showBusy')) $(".showProgress").attr('src',ASSETS_URL+'loading.gif');
 	if($(event.currentTarget).hasClass('confirm')) {
 		if(!confirm($(event.currentTarget).attr("title"))) { 
 			preventAjax = true; 
