727533a83fd38e7373684f72c1815c6d4325e88d
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2010-09-01 07:46:14 +0000

    added

---
+++ trunk/image.conf.php
@@ -0,0 +1,16 @@
+<?php
+class image_conf {
+var $libraryBasePath = '';
+var $sourceBasePath = 'obr/';
+var $targetBasePath = 'image/';
+var $contentType = 'image/jpeg';
+var $maxScaleUpRatio = 1.4;
+var $minNoScaleRatio = 0.8;
+var $maxNoScaleRatio = 1.2;
+var $sharpen = false;
+var $output = true;
+var $sideDefault = 600;
+var $sideOptions = '200,400,600,800,1000,1200,1400,1600';
+var $cutDefault = 'prop';
+var $cutOptions = 'prop,crop,flush';
+}
