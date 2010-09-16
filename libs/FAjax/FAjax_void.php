93addbacff37f78aaa8f31b28d3509352f0bab6b
kokokonec@gmail.com <kokokonec@gmail.com@200bec2e-8a23-11dd-b073-6769e66693fe>
2010-09-16 10:29:03 +0000

    js cleanup

--- libs/FAjax/FAjax_void.php
+++ libs/FAjax/FAjax_void.php
@@ -1,19 +1,13 @@
 <?php
 class FAjax_void extends FAjaxPluginBase {
 	static function markitup($data) {
-
-		FAjax::addResponse('function','css',URL_JS.'markitup/skins/simple/style.css?r='.rand());
-		FAjax::addResponse('function','css',URL_JS.'markitup/sets/default/style.css?r='.rand());
-		FAjax::addResponse('function','getScript',URL_JS.'markitup/jquery.markitup.pack.js');
-		FAjax::addResponse('function','getScript',URL_JS.'markitup/sets/default/set.js;markItUpInit');
-			
+		FAjax::addResponse('function','css',URL_JS.'markitup/skins/simple/style.css');
+		FAjax::addResponse('function','css',URL_JS.'markitup/sets/default/style.css');
+		FAjax::addResponse('function','getScript',URL_JS.'markitup/jquery.markitup.pack.js;markItUpInit');
 	}
 	static function datepicker($data) {
-		
-		FAjax::addResponse('function','getScript', URL_JS.'jquery-ui.datepicker.js;datePickerInit');
-		FAjax::addResponse('function','getScript',URL_JS.'i18n/ui.datepicker-cs.js');
 		FAjax::addResponse('function','css','css/themes/ui-lightness/jquery-ui-1.7.2.custom.css');
-		
+		FAjax::addResponse('function','getScript',URL_JS.'i18n/ui.datepicker-cs.js,datePickerInit');
 	}
 
 }
