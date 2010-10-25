04297f5c4166eb686b58459239c742ce9d8641d6
kokokonec <kokokonec@200bec2e-8a23-11dd-b073-6769e66693fe>
2010-10-25 08:04:29 +0000

    dev

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //local settings for current server
-define('ROOT', "C:/_web/fdk5/");
-define('WEBROOT', "C:/_web/fdk5/");
+define('ROOT', "D:/_web/fdk5/");
+define('WEBROOT', "D:/_web/fdk5/");
 define('INIT_FILENAME', ROOT.'system.init.php');
 define('PHPLOG_FILENAME', WEBROOT.'tmp/php.log');
 //---application index
