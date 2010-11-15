1c84fb5b3c832fc76c111dfa52ace8a3db1bda48
kokokonec@gmail.com <kokokonec@gmail.com@200bec2e-8a23-11dd-b073-6769e66693fe>
2010-11-15 15:18:15 +0000

    bugfixing

--- cron/invalidate.php
+++ cron/invalidate.php
@@ -1,6 +1,6 @@
 <?php
 if(!isset($_GET['invalidate'])) exit;
-$inv = explode('-',$_GET['invalidate']);
+$inv = explode('/',$_GET['invalidate']);
 $cache = FCache::getInstance('f');
 if(count($inv)>1) $cache->invalidateData($inv[1],$inv[0]);
 $cache->invalidateGroup($inv[0]);
