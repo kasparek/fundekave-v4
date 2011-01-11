c2518958c6d313d84fde7160c42fcb234623173d
kokokonec@gmail.com <kokokonec@gmail.com@200bec2e-8a23-11dd-b073-6769e66693fe>
2011-01-11 13:21:38 +0000

    fix

--- trunk/page/sidebar/Sidebar_galeryRand.php
+++ trunk/page/sidebar/Sidebar_galeryRand.php
@@ -1,16 +1,28 @@
 <?php
 class Sidebar_galeryRand {
 	static function show() {
-		$cache = FCache::getInstance('f',86400);
-		$itemIdList = $cache->getData('itemIdList'.(SITE_STRICT == 1 ? HOME_PAGE : ''),'sidebar');
+		$cache = FCache::getInstance('f');
+		$itemIdList = $cache->getData(HOME_PAGE.'fotorand','sidebar');
+				
 		if(empty($itemIdList)) {
-			$allList = FDBTool::getCol("select itemId from sys_pages_items where typeId='galery' and public='1'".(SITE_STRICT == 1 ? " and pageIdTop='".HOME_PAGE."'" : ''));
-			if(empty($allList)) return;
-			$keyList = array_rand($allList,500);
-			$itemIdList=array();
-			while($k = array_pop($keyList)) $itemIdList[]=$allList[$k];
-		}
-		$itemVO = new ItemVO(array_pop($itemIdList),false);
+			$allList = FDBTool::getCol("select itemId from sys_pages_items where typeId='galery' and public='1'".(SITE_STRICT == 1 ? " and pageIdTop='".HOME_PAGE."'" : '')." order by itemId desc");
+			
+			$num = 100;
+			$len = count($allList);
+			$steps = ceil($len/$num);
+			for($i=0;$i<$num;$i++){
+				$from = $i*$steps;
+				$rand = rand($from,$from+$steps>$len?$len:$from+$steps);
+				$itemIdList[]=$allList[$rand];
+			}
+			$itemIdList = array_reverse($itemIdList);
+		} 
+		
+		$itemId = array_pop($itemIdList);
+
+		$cache->setData($itemIdList,HOME_PAGE.'fotorand','sidebar');
+				
+		$itemVO = new ItemVO($itemId,false);
 		$itemVO->options=array('showPage'=>true);
 		$cache->setData($itemIdList,'itemIdList','sidebar');
 		return $itemVO->render().'<div style="clear:both;"></div>';
