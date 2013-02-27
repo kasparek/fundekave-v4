926f8f39af5b6b4458d320038c51e2700a3b7e0b
kokokonec@gmail.com <kokokonec@gmail.com@200bec2e-8a23-11dd-b073-6769e66693fe>
2013-02-27 13:49:25 +0000

    galleria removed, new javascript server

---
+++ trunk/js/bootstrap/_fdk.lng.js
@@ -0,0 +1,5 @@
+_fdk.lng = {
+ajax:{error:'Doslo k chybe pri spojeni, zkuste odeslat znovu'},
+goomapi:{title:'Vyberte více bodu pro zobrazení trasy',distance:'Vzdálenost: ',search:'Hledat',clearTitle:'Vymazat všechny souradnice',clear:'Smazat',saveTitle:'Ulož souradnice do formuláre',save:'Uložit',unitTitle:'Jednotky vzdálenosti - vzdálenost se pocítá mezi dvema a více souradnicemi na mape, zobrazena v horní lište okna s mapou',markerTitle:'Dvojklikem smazat, tažením zmenit polohu',close:'Zavrít'},
+galery:{selectFiles:'Vyberte fotografie',errorUpload:'Doslo k chybe pri nahravani, zkuste to znovu',errorFileNumLimit:'Vlozili jste priliz mnoho obrazku'}
+};
---
+++ trunk/js/index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('_compiler/JSCompiler.php');
+
+//input directory
+$baseDir = '';
+$dir = $baseDir . str_replace(array($baseDir,'..','.js'),'',$_GET['js']);
+
+//check if parameter is not missing
+if(empty($dir)) {
+	echo 'Missing parameter.';
+	exit;
+}
+
+//check if input directory exists
+if(!is_dir($dir)) {
+	echo 'Directory does not exists.';
+	exit;
+}
+
+//compiler 
+$c = new JSCompiler();
+$output = $c->addDir($dir)->simpleMode()->cacheDir($baseDir)->hideDebugInfo()->write();
