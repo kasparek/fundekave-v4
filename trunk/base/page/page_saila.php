e73578e5480ee0f305536a0ffbd87bdfb2acf008
kokokonec@gmail.com <kokokonec@gmail.com@200bec2e-8a23-11dd-b073-6769e66693fe>
2013-05-01 22:34:03 +0000

    sail web tweaks

--- trunk/base/page/page_saila.php
+++ trunk/base/page/page_saila.php
@@ -3,45 +3,42 @@ class page_saila  {
 
 	static function show($tpl) {
 	  
-    $fitems = new FItems('blog');
+		$fitems = new FItems('blog');
 		$fitems->joinOnPropertie('position',0,'join');
-    $fitems->addWhere('sys_pages_items.pageId="vd98H"');
-    $fitems->setOrder('itemId');
+		$fitems->addWhere('sys_pages_items.pageId="vd98H"');
+		$fitems->setOrder('dateStart desc');
 		$list = $fitems->getList(0,5);
     
-    $fitems = new FItems('blog');
+		$fitems = new FItems('blog');
 		$fitems->joinOnPropertie('position',0,'join');
-    $fitems->addWhere('sys_pages_items.pageId="DTMTH"');
-    $fitems->setOrder('sys_pages_items.pageId,itemId desc');
+		$fitems->addWhere('sys_pages_items.pageId="DTMTH"');
+		$fitems->setOrder('sys_pages_items.pageId,itemId desc');
 		$list = array_merge($list,$fitems->getList(0,5));
     
 		if(!empty($list))
 		while($itemVO = array_pop($list)) {
-		  
-      $journey = explode(';',$itemVO->prop('position'));
+			$journey = explode(';',$itemVO->prop('position'));
 			list($lat,$lng)=explode(',',$journey[count($journey)-1]);
-      
-      if($itemVO->pageId=='vd98H') {
-        //red
-        $markersRed[]=round($lat,4).','.round($lng,4);
-      } else {
-        //blue
-        $markersBlue[]=round($lat,4).','.round($lng,4);
-      }
-      
-      if(count($journey)>1) {
-					$geoEncode = new GooEncodePoly();
-          $paths[]='color:'.($itemVO->pageId=='vd98H'?'red':'blue').'|enc:'.$geoEncode->encode($journey);
+			if($itemVO->pageId=='vd98H') {
+				//red
+				$markersRed[]=round($lat,4).','.round($lng,4);
+			} else {
+				//blue
+				$markersBlue[]=round($lat,4).','.round($lng,4);
+			}
+			if(count($journey)>1) {
+				$geoEncode = new GooEncodePoly();
+				$paths[]='color:'.($itemVO->pageId=='vd98H'?'red':'blue').'|enc:'.$geoEncode->encode($journey);
 			}
 		}
     
-    $redStr="color:red|size:small|".implode('|',$markersRed);
-    $blueStr="color:blue|size:small|".implode('|',$markersBlue);
+		$redStr="color:red|size:small|".implode('|',$markersRed);
+		$blueStr="color:blue|size:small|".implode('|',$markersBlue);
     
-    //$tpl->setVariable('STATICMARKERPOS',implode('|',$markers));
-    //if(!empty($paths)) $tpl->setVariable('SWPLIST',implode('&path=',$paths));
-    $url = 'http://maps.google.com/maps/api/staticmap?size=170x170&markers='.$redStr.'&markers='.$blueStr.'&sensor=false&path='.implode('&path=',$paths);
-    $tpl->setVariable('GMAPURL',$url);
+		//$tpl->setVariable('STATICMARKERPOS',implode('|',$markers));
+		//if(!empty($paths)) $tpl->setVariable('SWPLIST',implode('&path=',$paths));
+		$url = 'http://maps.google.com/maps/api/staticmap?size=170x170&markers='.$redStr.'&markers='.$blueStr.'&sensor=false&path='.implode('&path=',$paths);
+		$tpl->setVariable('GMAPURL',$url);
 	}
   
 }
