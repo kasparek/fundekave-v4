ea6745f839d0d57adbdc067d4f448ec14caa53ce
kasparek <kaspar@fundekave.net>
2015-08-11 01:33:52 +1200

    version inc

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_GET['host'])) $host = $_GET['host'];
-define('VERSION', 'v01');
+define('VERSION', 'v02');
 define('WEBROOT', getcwd().'/');
 define('ROOT', getcwd().'/'.(file_exists(WEBROOT.'base') ? 'base/' : 'fdk_'.VERSION.'/'));
 define('PHPLOG_FILENAME', '/tmp/php.log');
