29c12eccd9cf2cb649c3aa968237ab96b31c2d8f
Frantisek Kaspar <kaspar@fundekave.net>
2018-09-28 00:50:07 +0100

    clean up

--- js/index.php
+++ js/index.php
@@ -66,29 +66,3 @@ if ($response && $response->isWithoutErrors()) {
     echo $code;
     echo "console.log(JSON.parse('".json_encode($errors)."'));";
 }
-
-/*
-
-require_once('_compiler/JSCompiler.php');
-
-//input directory
-$baseDir = '';
-$dir = $baseDir . str_replace(array($baseDir,'..','.js'),'',$_GET['js']);
-
-//check if parameter is not missing
-if(empty($dir)) {
-	echo 'Missing parameter.';
-	exit;
-}
-
-//check if input directory exists
-if(!is_dir($dir)) {
-	echo 'Directory does not exists.';
-	exit;
-}
-
-//compiler 
-$c = new JSCompiler();
-//$output = $c->addDir($dir)->simpleMode()->cacheDir($baseDir)->hideDebugInfo()->write();
-$output = $c->addDir($dir)->localCompile()->prettyPrint()->cacheDir($baseDir)->hideDebugInfo()->write();
-*/
